570afbe2d5e725a1a9f0502af2db5ca57ce1843a
MeetPujara <meetpujara02@gmail.com>
2025-03-04 22:54:32 +0530

    Update CORS origin to remove trailing slash for consistency

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 export const app = express();
 
 const corsOptions = {
-	origin: "http://localhost:5173/",
+	origin: ["http://localhost:5173"],
 	methods: ["POST","GET"],
 	credentials: true,
 };
