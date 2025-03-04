e7bc3c46848548f90bbbcdafb3ccff3b6310b40f
MeetPujara <meetpujara02@gmail.com>
2025-03-04 21:14:19 +0530

    Enhance backend error handling and configure CORS for specific origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,27 @@
-import express from 'express'
-import { router } from './routes/ai.routes.js'
-import cors from 'cors'
+import express from "express";
+import { router } from "./routes/ai.routes.js";
+import cors from "cors";
 
-export const app = express()
+export const app = express();
 
-app.use(cors())
+app.use(
+	cors({
+		origin:
+			"https://code-reviewer-ai-rq2w.vercel.app",
+		    methods: "GET,POST,OPTIONS",
+		    allowedHeaders: "Content-Type,Authorization",
+	})
+);
 
-app.use(express.json())
-app.use('/ai',router)
+app.use(express.json());
+app.use("/ai", router);
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Something broke!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
