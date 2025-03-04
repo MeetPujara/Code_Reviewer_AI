ac076f4f744a024e33208a5a4106e0a4bac17f68
MeetPujara <meetpujara02@gmail.com>
2025-03-04 22:51:14 +0530

    Update CORS origin to include trailing slash and change response to JSON

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 export const app = express();
 
 const corsOptions = {
-	origin: "http://localhost:5173",
+	origin: "http://localhost:5173/",
 	methods: ["POST","GET"],
 	credentials: true,
 };
@@ -13,7 +13,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", (req, res) => {
-	res.send("Working")
+	res.json("Working")
 })
 app.use("/ai", router);
 
