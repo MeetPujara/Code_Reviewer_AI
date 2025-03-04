ac278e2e8e70efb9cc57db512ea39d03db9dcd3c
MeetPujara <meetpujara02@gmail.com>
2025-03-04 22:47:10 +0530

    Refactor CORS configuration and remove unused Vercel files

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,12 +5,16 @@ import cors from "cors";
 export const app = express();
 
 const corsOptions = {
-	origin: "http://localhost:5173", // Update this to your frontend's origin
+	origin: "http://localhost:5173",
+	methods: ["POST","GET"],
 	credentials: true,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use("/", (req, res) => {
+	res.send("Working")
+})
 app.use("/ai", router);
 
 app.use((err, req, res, next) => {
