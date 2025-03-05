29acf53f7cadd0343b7037b52d8ca32cb7024925
MeetPujara <meetpujara02@gmail.com>
2025-03-05 13:07:52 +0530

    Update Vite config to change API proxy path from /api to /ai

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default {
 	server: {
 		proxy: {
-			"/api": {
-				target: "https://code-reviewer-ai-backend-pi.vercel.app", // Backend URL
+			"/ai": {
+				target: "https://code-reviewer-ai-backend-pi.vercel.app", 
 				changeOrigin: true,
 				secure: false,
 			},
