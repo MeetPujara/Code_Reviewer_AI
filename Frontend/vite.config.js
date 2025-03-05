8df6ece4cb5626b81c500d95ebcd83b73a213e60
MeetPujara <meetpujara02@gmail.com>
2025-03-05 12:56:04 +0530

    Remove Vercel configuration and update Vite config to add API proxy settings

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://your-backend-domain.com", // Backend URL
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+};
+
